7d8c560d1cf2daabc96f2226c7d89e67e0da7e40
Lynn <clhuang224@gmail.com>
2026-03-07 01:28:05 +0800

    fix(vitest config): typecheck

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,9 @@
-import { fileURLToPath } from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
+export default defineConfig(async (env) =>
+  mergeConfig(typeof viteConfig === 'function' ? await viteConfig(env) : viteConfig, {
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
