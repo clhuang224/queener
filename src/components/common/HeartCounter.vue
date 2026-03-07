0b8ea6938d43ded98cde124b7b5e1811e7f86dce
Lynn <clhuang224@gmail.com>
2026-03-07 22:20:18 +0800

    feat(HeartCounter): add HeartCounter component with props for maxHearts and hearts

---
+++ src/components/common/HeartCounter.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps({
+  maxHearts: {
+    type: Number,
+    default: 3,
+  },
+  hearts: {
+    type: Number,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="hearts">
+    <div class="heart" v-for="i in maxHearts" :key="i">
+      {{ i <= hearts ? '❤️' : '🤍' }}
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.hearts {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+  font-size: 40px;
+}
+</style>
